33d0213c443a5bd227171291f61a1009ffbb53c7
Chris Major (WLT GB) <chris.major@wlt.com>
2018-04-25 13:59:17 +0100

    basic logic added for stripping out last vowel

--- src/main/java/io/sistersdoingitforthemselves/company/controller/TechCompanyController.java
+++ src/main/java/io/sistersdoingitforthemselves/company/controller/TechCompanyController.java
@@ -1,5 +1,6 @@
 package io.sistersdoingitforthemselves.company.controller;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,11 @@ public class TechCompanyController {
 
     @GetMapping("/")
     public String getTechName(Model model) {
+        // here's the basic logic for stripping out the last vowel...
+        String thing = "not on my watch...";
+        int lastVowelIndex = StringUtils.lastIndexOfAny(thing, "a", "e", "i", "o", "u");
+        String newString = thing.substring(0, lastVowelIndex) + thing.substring(lastVowelIndex + 1);
+
         return "techname";
     }
 }
